3372ebb79e9ee2480e19ed6dd9ea664acd61b9d1
MiguelDeLasTorres <98329770+MiguelDeLasTorres@users.noreply.github.com>
2022-03-28 14:23:08 +0200

    Add files via upload

---
+++ helloworld/SampleController.java
@@ -0,0 +1,307 @@
+package helloworld;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+public class SampleController implements Initializable{
+
+	public ComboBox<String> attributs;
+	public ComboBox<String> valeurs;
+	public TextField textNom;
+	public TextField attributField;
+	public TextField valeurField;
+	public ImageView imagePreview;
+	public ImageView imageActive;
+	public ListView<String> valeurActive;
+	public Label labelNom;
+	public Label nbAttribut;
+	public Label status;
+	public Button attributV;
+	public Button valeurV;
+	public Button valideButton;
+	int indexPersonnage = 0;
+	DirectoryChooser directoryChooser = new DirectoryChooser();
+	ArrayList<String> listImages = new ArrayList<>();
+	ArrayList<Attribut> listAttribut = new ArrayList<>();
+	ArrayList<Valeur> listValeurs = new ArrayList<>();
+	Personnage[] listPersonnage;
+	JacksonParser parser = new JacksonParser();
+	
+	
+	/*
+	 * Set la liste d'image listImages avec les images présent dans le dossier sélectionné, et affiche la premier image
+	 */
+	@FXML
+	public void fileButtonClick() {
+		Stage stage = new Stage();
+		DirectoryChooser dc = new DirectoryChooser();		
+		File directory = dc.showDialog(stage);
+		String[] images = null;
+		imagePreview.setImage(null);
+		listImages.clear();
+		//On vérifie si un dossier est sélectionné puis si il y a des images
+		try {
+			try {
+				images = directory.list(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						return name.toLowerCase().endsWith(".png") || 
+								name.toLowerCase().endsWith(".jpg") ||
+								name.toLowerCase().endsWith(".jpeg") ||
+								name.toLowerCase().endsWith(".gif") ||
+								name.toLowerCase().endsWith(".bmp");
+					}
+				});
+				for (String string : images) {
+					listImages.add("file:"+directory.getAbsolutePath()+"\\"+string);
+				}
+				listPersonnage = new Personnage[listImages.size()];
+				Image first = new Image(listImages.get(indexPersonnage));
+				imagePreview.setImage(first);
+			}catch (IndexOutOfBoundsException e) {
+				status.setText("Le dossier sélectionné ne contient pas d'images !");
+			}
+		}catch (NullPointerException e) {
+			status.setText("Veuillez sélectionner un dossier !");
+		}
+	}
+	
+	/*
+	 * Flèche gauche et droite permettant de changer de personnage
+	 */
+	
+	public void leftArrowClick() {
+		Image image; 
+		if (!listImages.isEmpty()) {
+			if (indexPersonnage!=0) {indexPersonnage--;}
+			else {indexPersonnage=listImages.size()-1;}
+			try {
+				resetAllValue();
+				setPreview(listPersonnage[indexPersonnage]);//On vérifie si il y a un personnage à l'index demandé sinon on crée un page vierge
+				}
+			catch (NullPointerException e) {
+				resetAllValue();
+				}
+			image = new Image(listImages.get(indexPersonnage));
+			imagePreview.setImage(image);
+		}
+		
+	}
+	
+	public void rightArrowClick() {
+		Image image;
+		if (!listImages.isEmpty()) {
+			if (indexPersonnage!=listImages.size()-1) {indexPersonnage++;}
+			else {indexPersonnage = 0;}
+			try {
+				resetAllValue();
+				setPreview(listPersonnage[indexPersonnage]);//On vérifie si il y a un personnage à l'index demandé sinon on crée un page vierge
+				}
+			catch (NullPointerException e) {
+				resetAllValue();
+				}
+			image = new Image(listImages.get(indexPersonnage));
+			imagePreview.setImage(image);
+		}
+	}
+	
+	/*
+	 * Les boutons pour valider et supprimer les valeurs et les attributs
+	 */
+	
+	public void attributVClick() {
+		if (attributField.getText().length()>0 && !attributs.getItems().contains(attributField.getText())) {
+			attributs.getItems().add(attributField.getText());
+			listAttribut.add(new Attribut(attributField.getText()));
+			attributs.getSelectionModel().select(attributField.getText());
+			attributField.clear();
+		}
+	}
+	
+	public void attributClear() {
+		if (attributs.getValue()!=null) {
+			listAttribut.remove(Attribut.getAttribut(listAttribut, attributs.getValue()));
+			attributs.getItems().remove(attributs.getSelectionModel().getSelectedItem());
+		}
+	}
+	
+	public void valeurVClick() {
+		if (valeurField.getText().length()>0 && !valeurs.getItems().contains(valeurField.getText())) {
+			valeurs.getItems().add(valeurField.getText());
+			Attribut.getAttribut(listAttribut, attributs.getValue()).addValeur(valeurField.getText());
+			valeurs.getSelectionModel().select(valeurField.getText());
+			valeurField.clear();
+		}
+	}
+	
+	public void valeurClear() {
+		if (valeurs.getValue()!=null) {
+			Attribut.getAttribut(listAttribut, attributs.getValue()).delValeur(valeurs.getValue());
+			valeurs.getItems().remove(valeurs.getSelectionModel().getSelectedItem());
+		}
+	}
+	
+	public void nomFill() {
+		if (valeurs.getValue()!=null && textNom.getText().length()>0) {
+			valideButton.setDisable(false);
+		}else {
+			valideButton.setDisable(true);
+		}
+	}
+	
+	public void attributsCBClick() {
+		if (attributs.getValue()!=null) {
+			valeurV.setDisable(false);
+		valeurs.getItems().clear();
+		if (!listAttribut.isEmpty()) {
+			Attribut att = Attribut.getAttribut(listAttribut, attributs.getValue());
+			for (String str : att.getValeurs()) {
+				valeurs.getItems().add(str);
+			}
+		}
+		}else {
+			valeurV.setDisable(true);
+		}
+	}
+	
+	public void valeursCBClick() {
+		if (valeurs.getValue()!=null && textNom.getText().length()>0) {
+			valideButton.setDisable(false);
+		}else {
+			valideButton.setDisable(true);
+		}
+	}
+	
+	/*
+	 * Pour ajouter les valeurs au tableau de valeurs associées au personnage sélectionné.
+	 */
+	
+	public void setPreview(Personnage pers) {
+		resetPreview();
+		textNom.setText(pers.getName());
+		imageActive.setImage(new Image(pers.getFilepath()));
+		labelNom.setText(pers.getName());
+		for (Valeur val : pers.getValeurs()) {
+			valeurActive.getItems().add(val.getClef()+"  :  "+val.getValeur());
+		}
+		nbAttribut.setText(pers.getValeurs().size()+"/"+listAttribut.size());
+	}
+	
+	public void resetPreview() {
+		imageActive.setImage(null);
+		labelNom.setText("");
+		valeurActive.getItems().clear();
+		
+	}
+	
+	public void resetValue() {
+		valeurs.getItems().clear();
+		attributField.clear();
+		valeurField.clear();
+		valeurV.setDisable(true);
+		valideButton.setDisable(true);
+		attributs.getSelectionModel().select(-1);
+	}
+	
+	public void resetAllValue() {
+		resetValue();
+		resetPreview();
+		textNom.clear();
+		listValeurs.clear();
+		nbAttribut.setText("");
+	}
+	
+	public boolean checkAttribut() {
+		for (Personnage pers : listPersonnage) {
+			if (pers==null) {
+				return false;
+			}
+			if (pers.getValeurs().size()!=listAttribut.size()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean checkValidity() {
+		for (int i = 0; i < listPersonnage.length; i++) {
+			ArrayList<Valeur> valeursI = listPersonnage[i].getValeurs();
+			for (int j = i+1; j < listPersonnage.length; j++) {
+				ArrayList<Valeur> valeursJ = listPersonnage[j].getValeurs();
+				if (Valeur.equalsArray(valeursJ, valeursI)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public void valideButton() {
+		if (!listImages.isEmpty()) {
+			status.setText("Ajout des valeurs");
+			Valeur val = new Valeur(attributs.getValue(), valeurs.getValue());
+			listValeurs.add(val);
+			if (listPersonnage[indexPersonnage]==null) {
+				Personnage personnage = new Personnage(textNom.getText(), listImages.get(indexPersonnage), listValeurs);
+				listPersonnage[indexPersonnage] = personnage;
+			}else {
+				if (Valeur.getValeurFromArray(listPersonnage[indexPersonnage].getValeurs(),val.getClef())==null) {	
+					listPersonnage[indexPersonnage].getValeurs().add(val);
+				}else {
+					listPersonnage[indexPersonnage].getValeurFromClef(val.getClef()).setValeur(val.getValeur());
+				}
+			}
+			setPreview(listPersonnage[indexPersonnage]);
+			resetValue();
+		}else {
+			status.setText("Vous n'avez pas sélectionné de dossier");
+		}
+	}
+	
+	public void cancelButton() {
+		resetValue();
+		valeurV.setDisable(false);
+	}
+	
+	public void saveButton() {
+		try {
+			if (checkAttribut() && checkValidity()) {
+				try {
+					parser.EnregistrerAttributs("Attribut.json", listAttribut);
+					parser.EnregistrerPersonnages("Personnage.json", listPersonnage);
+					status.setText("Vous avez sauvegardé !");
+				}catch (IOException e){
+					status.setText("Erreur lors de la sauvegarde !");
+					e.printStackTrace();
+				}
+			}else {
+				status.setText("Vous n'avez pas complété les caractèristiques de chaque personnages !");
+			}
+		}catch(NullPointerException e) {
+			status.setText("Vous n'avez rien à sauvegarder !");
+		}
+		
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
